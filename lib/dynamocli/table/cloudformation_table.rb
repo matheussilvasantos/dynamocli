c98634a0b332604450e0a58c3855b5543bdb6203
Matheus Silva Santos de Oliveira <oliveira.matheussilva@outlook.com>
2020-01-03 07:39:13 +0000

    Create a Stack class to deal with stack information

--- lib/dynamocli/table/cloudformation_table.rb
+++ lib/dynamocli/table/cloudformation_table.rb
@@ -9,20 +9,20 @@ module Dynamocli::Table
   class CloudformationTable
     CLOUDFORMARTION = Aws::CloudFormation::Client
     LOGGER = TTY::Logger
+    DEPLOY_COMPLETED_KEY = "UPDATE_COMPLETE"
 
-    def initialize(table_name:, table_resource:, cloudformation: nil, logger: nil)
+    def initialize(table_name:, stack:, cloudformation: nil, logger: nil)
       @table_name = table_name
-      @table_resource = table_resource
+      @stack = stack
       @cloudformation = cloudformation ||= CLOUDFORMARTION.new
       @logger = logger ||= LOGGER.new
     end
 
     def alert_message_before_continue
-      "You're going to deploy and redeploy your #{@stack_name} stack to drop and recreate the #{@table_name} table!"
+      "You're going to deploy and redeploy your #{stack.name} stack to drop and recreate the #{@table_name} table!"
     end
 
     def erase
-      set_all_stack_information_before_we_change_it
       deploy_stack_without_the_table
       wait_for_deployment_to_complete
       deploy_stack_with_the_original_template
@@ -33,76 +33,19 @@ module Dynamocli::Table
 
     private
 
-    attr_reader :table_name, :table_resource, :cloudformation, :logger, :stack, :stack_name,
-                :stack_resources, :template_body, :original_template, :template_without_table,
-                :stack_policy_body
-
-    def set_all_stack_information_before_we_change_it
-      set_stack
-      set_stack_name
-      set_stack_resources
-      set_template_body
-      set_original_template
-      set_template_without_table
-      set_stack_policy_body
-    end
-
-    def set_stack
-      @stack ||= cloudformation.describe_stacks(stack_name: stack_name)[0][0]
-    end
-
-    def set_stack_name
-      @stack_name ||= table_resource[:stack_name]
-    end
-
-    def set_stack_resources
-      @stack_resources ||= cloudformation.describe_stack_resources(physical_resource_id: table_name).to_h
-    end
-
-    def set_template_body
-      @template_body ||= @cloudformation.get_template(stack_name: @stack_name).to_h[:template_body]
-    end
-
-    def set_original_template
-      @original_template ||= parse_template(template_body)
-    end
-
-    def set_template_without_table
-      @template_without_table ||= parse_template(template_body).tap do |template_without_table|
-        tables = original_template["Resources"].select { |_, v| v["Type"] == "AWS::DynamoDB::Table" }
-        table = tables.find { |_, v| v["Properties"]["TableName"] == @table_name }
-
-        if tables.nil?
-          logger.error("table #{@table_name} not found in the #{@stack_name} stack")
-          exit(42)
-        end
-
-        logical_resource_id = table.first
-        template_without_table["Resources"].delete(logical_resource_id)
-      end
-    end
-
-    def parse_template(template)
-      JSON.parse(template)
-    rescue JSON::ParserError
-      YAML.load(template)
-    end
-
-    def set_stack_policy_body
-      @stack_policy_body ||= cloudformation.get_stack_policy(stack_name: stack_name).stack_policy_body
-    end
+    attr_reader :table_name, :stack, :cloudformation, :logger
 
     def deploy_stack_without_the_table
       logger.info("Deploying the stack without the #{table_name} table")
 
       cloudformation.update_stack(
-        stack_name: stack_name,
-        template_body: template_without_table.to_json,
+        stack_name: stack.name,
+        template_body: stack.template_without_table.to_json,
         parameters: stack.parameters.map(&:to_h),
         capabilities: stack.capabilities,
         role_arn: stack.role_arn,
         rollback_configuration: stack.rollback_configuration.to_h,
-        stack_policy_body: stack_policy_body,
+        stack_policy_body: stack.policy_body,
         notification_arns: stack.notification_arns,
         tags: stack.tags.map(&:to_h)
       )
@@ -112,27 +55,23 @@ module Dynamocli::Table
 
     def wait_for_deployment_to_complete
       waiting_seconds = 0
-      while get_stack_status != "UPDATE_COMPLETE"
+      while stack.current_status != DEPLOY_COMPLETED_KEY
         logger.info("Waiting for deployment to complete")
         sleep waiting_seconds += 1
       end
-    end
-
-    def get_stack_status
-      cloudformation.describe_stacks(stack_name: stack_name)[0][0].stack_status
     end
 
     def deploy_stack_with_the_original_template
       logger.info("Deploying the stack with the #{table_name} table")
 
       cloudformation.update_stack(
-        stack_name: stack_name,
-        template_body: original_template.to_json,
+        stack_name: stack.name,
+        template_body: stack.original_template.to_json,
         parameters: stack.parameters.map(&:to_h),
         capabilities: stack.capabilities,
         role_arn: stack.role_arn,
         rollback_configuration: stack.rollback_configuration.to_h,
-        stack_policy_body: stack_policy_body,
+        stack_policy_body: stack.policy_body,
         notification_arns: stack.notification_arns,
         tags: stack.tags.map(&:to_h)
       )
